8a4207222267434323f3357c6810bac812c51784
Andreza Zeverino <82824036+AndrezaZeverino@users.noreply.github.com>
2021-08-24 11:05:22 -0300

    Add files via upload

---
+++ P11-CalculaPoupanca2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P11_CalculaPoupanca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 11!");
+
+            double valorInvestido = 1000;
+
+
+            for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá " +
+                    valorInvestido);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
