71c417dc9a9cd4305246d4f3eab580b3b724b5d5
Andreza Zeverino <82824036+AndrezaZeverino@users.noreply.github.com>
2021-08-24 11:02:46 -0300

    Add files via upload

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5. Caracteres e Textos");
+
+            char primeiraLetra = 'a';
+            Console.WriteLine("A letra é " + primeiraLetra);
+
+            string texto = "Aprendendo caracter e textos com CSharp";
+            Console.WriteLine(texto);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
