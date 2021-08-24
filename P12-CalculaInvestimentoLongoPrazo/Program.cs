395c8e51abe7b87a4af4f107bd4212fd10cd0ac3
Andreza Zeverino <82824036+AndrezaZeverino@users.noreply.github.com>
2021-08-24 11:06:27 -0300

    Add files via upload

---
+++ P12-CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _12__CalculaInvestimentoLongoPrazo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executanto o projeto 12 - calculo de investimento a longo prazo");
+
+
+            double valorInvestido = 1000;
+            double fatorRendimento = 1.0036;
+
+            for (int contadorAno = 1; contadorAno <= 5; contadorAno++)
+            //Contagem de anos 
+            {
+                //Contagem de meses 
+                for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+                {
+                    valorInvestido *= fatorRendimento;
+                }
+                fatorRendimento += 0.0010;
+            }
+
+            Console.WriteLine("Ao término do investimento, você terá R$" + valorInvestido);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P13-ForEncadeado/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 13");
+
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                        break;
+                }
+                //quando coloco o Console.WriteLine sem nada de texto, ele apenas pula linha
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
