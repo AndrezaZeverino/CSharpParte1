53ca92f22caf976fb829e7b67d1c006881f4cf5e
Andreza Zeverino <82824036+AndrezaZeverino@users.noreply.github.com>
2021-08-24 11:03:56 -0300

    Add files via upload

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("testando condicionais");
+
+            int idade = 20;
+            int quantidadePessoas = 2;
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Você tem mais que 18 anos");
+                Console.WriteLine("Seja bem-vindo");
+            }
+            else
+            {
+                if (quantidadePessoas >= 2)
+                {
+                    Console.WriteLine("você não tem 18, mas pode entrar, pois está acompanhado");
+
+                }
+                else
+                {
+                    Console.WriteLine("infelizmente você não pode entrar");
+                }
+
+            }
+        }
+    }
+}
---
+++ 8 - Condicionais2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _8___Condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("8 - testando condicionais 2");
+
+            int idade = 19;
+            int quantidadePessoas = 2;
+
+            bool acompanhado = quantidadePessoas >= 2;
+
+            if (idade >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar");
+
+            }
+
+            else
+            {
+                Console.WriteLine("Não pode entrar");
+            }
+        }
+    }
+}
